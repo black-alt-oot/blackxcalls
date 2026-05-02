6d642d03b59943475d846f78bbe47a0ca14c43b2
black-alt-oot <meshmaga@gmail.com>
2026-05-02 14:25:08 +0100

    chore: sync all source files for Vercel build

---
+++ artifacts/relationships/src/App.tsx
@@ -0,0 +1,24 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import Home from "@/pages/home";
+import Admin from "@/pages/admin";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/admin" component={Admin} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <Router />
+    </WouterRouter>
+  );
+}
+
+export default App;
